dec1679c1eefc436906c54cf20a31a25c83597e4
sergio-macedo <you@example.com>
2024-09-21 00:12:53 +0200

    feat: created the iam role for ec2

--- iam.tf
+++ iam.tf
@@ -1,18 +1,49 @@
+# Create IAM Role for EC2 to access ECR
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ecr-access-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
+# IAM Policy to allow EC2 to interact with ECR
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ec2-ecr-access-policy"
+  description = "Allow EC2 to pull images from ECR"
 
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "ecr:GetDownloadUrlForLayer",
-#         "ecr:BatchGetImage",
-#         "ecr:BatchCheckLayerAvailability",
-#         "ecr:GetAuthorizationToken"
-#       ],
-#       "Resource": "*"
-#     }
-#   ]
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+      {
+        Action   = "ecr:GetAuthorizationToken",
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the ECR policy to the IAM Role
+resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
