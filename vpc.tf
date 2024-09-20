2b3aadd18d5322de409da5b3709d6a8eeb5a0a5e
sergio-macedo <you@example.com>
2024-09-20 12:19:53 +0200

    feat: addded igw and rtb

--- vpc.tf
+++ vpc.tf
@@ -6,5 +6,24 @@ resource "aws_vpc" "cgi_vpc" {
     Name = "CGI-Challenge"
   }
 
+}
 
+resource "aws_internet_gateway" "cgi_igw" {
+  vpc_id = aws_vpc.cgi_vpc.id
+  tags = {
+    Name = "cgi-IGW"
+  }
+}
+
+resource "aws_route_table" "cgi_pub_rtb" {
+  vpc_id = aws_vpc.cgi_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cgi_igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.CGI_challenge_pub_subnet.id
+  route_table_id = aws_route_table.cgi_pub_rtb.id
 }
