468a8f86164e1e93faf86477975882aa80d4d14d
sergio-macedo <sergio@LAPTOP-QAF5LOGO>
2024-09-23 00:52:22 +0200

    refactor:providers and backend

---
+++ backend.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  required_version = "~>  1.7"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
---
+++ modules.tf
@@ -0,0 +1,16 @@
+# module "network" {
+#     source = "./modules/network"
+#     tags = local.tags
+#     project_name = var.project_name
+#     cidr_block = var.cidr_block
+#     aws_region = var.aws_region
+
+# }
+# module "ecs" {
+#         source = "./modules/ecs"
+#         project_name = var.project_name
+#         tags = local.tags
+
+# }
+
+  
