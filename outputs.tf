7a3b08e0dee6de5e795a26ece4597d8f93b45db2
sergio-macedo <sergio@LAPTOP-QAF5LOGO>
2024-09-23 00:59:28 +0200

    feat: new server for the container for testing

---
+++ outputs.tf
@@ -0,0 +1,34 @@
+output "vpc" {
+  value = {
+    id = aws_vpc.cgi_vpc.id
+    arn = aws_vpc.cgi_vpc.arn
+    cidr_block = aws_vpc.cgi_vpc.cidr_block
+  }
+  
+}
+
+output "subnets" {
+  value = {
+    private = {
+      id = aws_subnet.CGI_challenge_priv_subnet.*.id
+      cidr_block = aws_subnet.CGI_challenge_priv_subnet.*.cidr_block
+    }
+    public = {
+      id = aws_subnet.CGI_challenge_pub_subnet.*.id
+      cidr_block = aws_subnet.CGI_challenge_pub_subnet.*.cidr_block
+    }
+  }
+  
+}
+
+output "route_tables" {
+  value = {
+    private = {
+      id = aws_route_table.cgi_priv_rtb.*.id
+    }
+    public = {
+      id = aws_route_table.cgi_pub_rtb.*.id
+    }
+  }
+  
+}
